85a73df29d7e6a2a372aecb2e1b4c22247f5c731
Anh Nguyen <74972572+anhann@users.noreply.github.com>
2022-03-06 15:32:27 +0000

    Update games_to_streamlit.py

--- games_to_streamlit.py
+++ games_to_streamlit.py
@@ -18,32 +18,31 @@ year,month,date=[int(i) for i in dob.split()]
 import datetime
 import lunarcalendar
 from lunarcalendar import Converter, Solar, Lunar
-def moon_phase():
-  solar = Solar(dob)
-  lunar = Converter.Solar2Lunar(solar)
-  if lunar.day <=1:
-    phase='New Moon'
-    char='Creative, Adventurous, Impulsive'
-  elif lunar.day <7:
-    phase='Waxing Crescent'
-    char= 'Ambitious, Diligent, Risk averse'
-  elif lunar.day <9:
-    phase='First Quarter'
-    char='Talented, Brave, Patient'
-  elif lunar.day <14:
-    phase='Waxing Gibbous'
-    char= 'Calm, Gracious, Perfectionist'
-  elif lunar.day <17:
-    phase='Full Moon'
-    char= 'Sensitive, Empathetic, Emotional'
-  elif lunar.day <22:
-    phase='Waning Gibbous'
-    char='Meditative, Analytical, Judgmental'
-  elif lunar.day <24:
-    phase='Third Quarter'
-    char='Loyal, Emotional, Sociable'
-  elif lunar.day >=24:
-    phase='Waning Crescent'
-    char= 'Imaginative, Divergent, Lonesome'
-  st.write ('Hi {}, your Moon phase is \033[32m {}\033[30m, and your characteristics are \033[32m {}\033[30m.'.format(name,phase,char))
+solar = Solar(dob)
+lunar = Converter.Solar2Lunar(solar)
+if lunar.day <=1:
+phase='New Moon'
+char='Creative, Adventurous, Impulsive'
+elif lunar.day <7:
+phase='Waxing Crescent'
+char= 'Ambitious, Diligent, Risk averse'
+elif lunar.day <9:
+phase='First Quarter'
+char='Talented, Brave, Patient'
+elif lunar.day <14:
+phase='Waxing Gibbous'
+char= 'Calm, Gracious, Perfectionist'
+elif lunar.day <17:
+phase='Full Moon'
+char= 'Sensitive, Empathetic, Emotional'
+elif lunar.day <22:
+phase='Waning Gibbous'
+char='Meditative, Analytical, Judgmental'
+elif lunar.day <24:
+phase='Third Quarter'
+char='Loyal, Emotional, Sociable'
+elif lunar.day >=24:
+phase='Waning Crescent'
+char= 'Imaginative, Divergent, Lonesome'
+st.write ('Hi {}, your Moon phase is \033[32m {}\033[30m, and your characteristics are \033[32m {}\033[30m.'.format(name,phase,char))
 
