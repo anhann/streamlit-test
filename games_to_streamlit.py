33a021af7c75d9fa8ae32cb8e6489ac0e4c0ebb9
Anh Nguyen <74972572+anhann@users.noreply.github.com>
2022-03-06 15:46:01 +0000

    Update games_to_streamlit.py

--- games_to_streamlit.py
+++ games_to_streamlit.py
@@ -9,40 +9,43 @@ Original file is located at
 import streamlit as st
 import datetime
 from lunarcalendar import Converter, Solar, Lunar
-st.title('Check your Moon phase')
-st.write('Please input your name')
-name=st.text_input('Name')
-st.write('Please input your date of birth (year, month, day), for ex: 1996 05 28')
-#dob=st.text_input('Date of birth')
-year,month,date=[int(i) for i in st.text_input('Date of birth').split()]
-import datetime
-import lunarcalendar
-from lunarcalendar import Converter, Solar, Lunar
-solar = Solar(year, month, date)
-lunar = Converter.Solar2Lunar(solar)
-if lunar.day <=1:
-    phase='New Moon'
-    char='Creative, Adventurous, Impulsive'
-elif lunar.day <7:
-    phase='Waxing Crescent'
-    char= 'Ambitious, Diligent, Risk averse'
-elif lunar.day <9:
-    phase='First Quarter'
-    char='Talented, Brave, Patient'
-elif lunar.day <14:
-    phase='Waxing Gibbous'
-    char= 'Calm, Gracious, Perfectionist'
-elif lunar.day <17:
-    phase='Full Moon'
-    char= 'Sensitive, Empathetic, Emotional'
-elif lunar.day <22:
-    phase='Waning Gibbous'
-    char='Meditative, Analytical, Judgmental'
-elif lunar.day <24:
-    phase='Third Quarter'
-    char='Loyal, Emotional, Sociable'
-elif lunar.day >=24:
-    phase='Waning Crescent'
-    char= 'Imaginative, Divergent, Lonesome'
-st.write ('Hi {}, your Moon phase is {}, and your characteristics are {}'.format(name,phase,char))
+try:
+    st.title('Check your Moon phase')
+    st.write('Please input your name')
+    name=st.text_input('Name')
+    st.write('Please input your date of birth (year, month, day), for ex: 1996 05 28')
+    #dob=st.text_input('Date of birth')
+    year,month,date=[int(i) for i in st.text_input('Date of birth').split()]
+    import datetime
+    import lunarcalendar
+    from lunarcalendar import Converter, Solar, Lunar
+    solar = Solar(year, month, date)
+    lunar = Converter.Solar2Lunar(solar)
+    if lunar.day <=1:
+        phase='New Moon'
+        char='Creative, Adventurous, Impulsive'
+    elif lunar.day <7:
+        phase='Waxing Crescent'
+        char= 'Ambitious, Diligent, Risk averse'
+    elif lunar.day <9:
+        phase='First Quarter'
+        char='Talented, Brave, Patient'
+    elif lunar.day <14:
+        phase='Waxing Gibbous'
+        char= 'Calm, Gracious, Perfectionist'
+    elif lunar.day <17:
+        phase='Full Moon'
+        char= 'Sensitive, Empathetic, Emotional'
+    elif lunar.day <22:
+        phase='Waning Gibbous'
+        char='Meditative, Analytical, Judgmental'
+    elif lunar.day <24:
+        phase='Third Quarter'
+        char='Loyal, Emotional, Sociable'
+    elif lunar.day >=24:
+        phase='Waning Crescent'
+        char= 'Imaginative, Divergent, Lonesome'
+    st.write ('Hi {}, your Moon phase is {}, and your characteristics are {}'.format(name,phase,char))
+except:
+    pass
 
