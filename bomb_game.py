ec8487d6c2b807faed64011c7700005627afa161
Anh Nguyen <74972572+anhann@users.noreply.github.com>
2022-03-13 13:01:19 +0000

    Update bomb_game.py

--- bomb_game.py
+++ bomb_game.py
@@ -12,7 +12,7 @@ def color_code(val):
         return f'background-color: {color}'
 st.write('To play, input the number of column & row you want (>=3 or else you could not win:))')
 n=st.number_input('Pick a number',3,99)
-st.write('This is a simple bomb game. The number of column and row is {} each. There will be {} bombs in the field.\nWhenever you input a row & column number (for ex 3 4 - remember to include space between 2 numbers!), I will inform you on the bomb risk.\nIn all cases, D letter means Danger zone, X letter mean Safer zone\nIf you can survive {} times of input, you win!'.format(n,n,n*2))
+st.write('This is a simple bomb game. The number of column and row is {} each. There will be {} bombs in the field.\nWhenever you input a row & column number (for ex 3 4 - remember to include space between 2 numbers!), I will inform you on the bomb risk.\nIn all cases, D letter means Danger zone where you are close to bomb, X letter mean Safer zone\nIf you can survive {} times of input, you win!'.format(n,n,n*2))
 st.write('\nPlease choose the difficulty level. Input 1 for harder mode, which means there is only alert if you near 2 bombs and above.\n2 is for easier mode, means I will inform the number of bomb around your input area')
 m=st.number_input('Pick a number',1,2)
 import random
@@ -69,7 +69,7 @@ if m==2:
                 st.write('Congratulation! You win')
 if m==1:
         while len(input_list)<n*2:
-                x=st.text_input('Please input your row & column numbers the {} time- please include space between 2 numbers'.format(len(input_list))+1)
+                x=st.text_input('Please input your row & column numbers - please include space between 2 numbers', key = text_input_list[len(input_list)])
                 l=[int(i) for i in x.split()]
                 if l[0]>n-1 or l[1]>n-1 or l[0]<0 or l[1]<0:
                         st.write('Input out of table range')
