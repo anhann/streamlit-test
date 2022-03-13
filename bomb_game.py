8e6d31031b325bfca2be66b3da0e36d718817f87
Anh Nguyen <74972572+anhann@users.noreply.github.com>
2022-03-13 12:15:20 +0000

    Update bomb_game.py

--- bomb_game.py
+++ bomb_game.py
@@ -30,8 +30,9 @@ try:
         # playing part:
         if m==2:
                 while len(input_list)<n*2:
-                        #x=st.text_input('Please input your row & column numbers - please include space between 2 numbers')
-                        l=[int(i) for i in st.text_input('Please input your row & column numbers - please include space between 2 numbers').split()]
+                        time.sleep(3)
+                        x=st.text_input('Please input your row & column numbers - please include space between 2 numbers')
+                        l=[int(i) for i in x.split()]
                         if l[0]>n-1 or l[1]>n-1 or l[0]<0 or l[1]<0:
                                 st.write('Input out of table range')
                                 continue
@@ -59,7 +60,7 @@ try:
                         #print (' '.join(col))
                         #st.dataframe(a.style.applymap(color_code))
                         st.dataframe(a)
-                        st.write('Fighting! Only {} more times to win'.format(n*2-len(input_list)))
+                        st.write('Fighting! Only {} more times to win'.format(n*2-len(input_list)))                      
                         continue
                 #st.dataframe(a.style.applymap(color_code))
                 st.dataframe(a)
