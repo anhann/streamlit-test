5f8b6fdd6f3af7ab52ff8c070c767b1097d19242
Anh Nguyen <74972572+anhann@users.noreply.github.com>
2022-03-13 12:36:34 +0000

    Update bomb_game.py

--- bomb_game.py
+++ bomb_game.py
@@ -31,8 +31,9 @@ try:
         # playing part:
         if m==2:
                 while len(input_list)<n*2:
-                        text_input_list[len(input_list)]=st.text_input('Please input your row & column numbers the {} time - please include space between 2 numbers')
-                        l=[int(i) for i in text_input_list[len(input_list)].split()]
+                        text_input_list[len(input_list)]=st.text_input('Please input your row & column numbers - please include space between 2 numbers')
+                        x=text_input_list[len(input_list)]
+                        l=[int(i) for i in x.split()]
                         if l[0]>n-1 or l[1]>n-1 or l[0]<0 or l[1]<0:
                                 st.write('Input out of table range')
                                 continue
